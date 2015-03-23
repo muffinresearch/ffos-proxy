fcd44b172ef54df25bf89cdc581c287f76f82696
Stuart Colville <scolville@mozilla.com>
2015-03-23 20:30:52 +0000

    Setup Gruntfile

---
+++ Gruntfile.js
@@ -0,0 +1,28 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    jshint: {
+      all: ['Gruntfile.js', 'lib/**/*.js', 'test/**/*.js', 'bin/proxify'],
+    },
+    mochaTest: {
+      options: {
+        require: [
+          function(){
+            /* jshint -W020 */
+            assert = require('chai').assert;
+          },
+        ],
+        reporter: 'spec',
+      },
+      all: ['tests/*.js']
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-mocha-test');
+
+  grunt.registerTask('default', ['jshint']);
+  grunt.registerTask('test', ['jshint', 'mochaTest']);
+};
